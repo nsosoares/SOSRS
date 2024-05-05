f0fc4db5cf4767940b5b9ef47e29d73fff8bc5d3
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2024-05-05 18:30:59 -0300

    Numero e complemento no response

--- src/SOSRS.Api/ViewModels/FiltroAbrigoResponseViewModel.cs
+++ src/SOSRS.Api/ViewModels/FiltroAbrigoResponseViewModel.cs
@@ -12,6 +12,8 @@ public class AbrigoResponseViewModel
     public string Nome { get; set; } = default!;
     public string Cidade { get; set; } = default!;
     public string Bairro { get; set; } = default!;
+    public int Numero { get; set; } = default!;
+    public string Complemento { get; set; } = default!;
     public string TipoChavePix { get; set; } = default!;
     public string ChavePix { get; set; } = default!;
     public EStatusCapacidade Capacidade { get; set; } = default!;
