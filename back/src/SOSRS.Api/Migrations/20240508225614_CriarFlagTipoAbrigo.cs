3815ed43c163bfff8b9af824dd50434517f66433
Juan Lopes <v.baires.dev1@sun.auto>
2024-05-08 19:57:26 -0300

    Implement

---
+++ back/src/SOSRS.Api/Migrations/20240508225614_CriarFlagTipoAbrigo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOSRS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarFlagTipoAbrigo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AbrigoDeAnimais",
+                table: "Abrigos",
+                newName: "PermiteAnimais");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "TipoAbrigo",
+                table: "Abrigos",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TipoAbrigo",
+                table: "Abrigos");
+
+            migrationBuilder.RenameColumn(
+                name: "PermiteAnimais",
+                table: "Abrigos",
+                newName: "AbrigoDeAnimais");
+        }
+    }
+}
